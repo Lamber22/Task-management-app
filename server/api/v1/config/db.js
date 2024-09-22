f6cb68fec7e7bb290dd36051a3fcbda4485e442e
Lamber22 <Lamberjoe5@gmail.com>
2024-09-22 17:23:59 +0000

    feat(DB): Database connection setup

---
+++ server/api/v1/config/db.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const DB = process.env.DB_URI.replace("<PASSWORD>", process.env.DB_PASSWORD);
+
+class DBClient {
+    constructor() {
+        this.connectDB();
+    }
+
+    async connectDB() {
+        try {
+        await mongoose.connect(DB);
+        console.log("Database connected successfully!!!");
+        } catch (error) {
+        console.error("Error connecting to the database:", error);
+        }
+    }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
