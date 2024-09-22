41777e6d0fe97e22d384325cfc6f8501c6654c81
Lamber22 <Lamberjoe5@gmail.com>
2024-09-22 17:33:00 +0000

    feat(Add Auth Controller): Register, Login, and Logout functionality

---
+++ server/api/v1/controllers/authController.js
@@ -0,0 +1,67 @@
+// controllers/authController.js
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+import generateToken from '../utils/generateToken.js';
+
+// Register User
+export const register = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' });
+        }
+
+        user = new User({ email, password });
+        await user.save();
+
+        res.status(201).json({
+            status: 'Success',
+            message: 'User registered successfully',
+            data: user,
+        });
+    } catch (err) {
+        res.status(500).json({ msg: 'Server error' });
+    }
+};
+
+// Login User
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+
+        const token = generateToken(user._id);  // Use generateToken function
+        res.status(200).json({
+            status: 'Success',
+            message: 'User logged in successfully',
+            token: token
+        });
+    } catch (err) {
+        res.status(500).json({ msg: 'Server error' });
+    }
+};
+
+// user logOut handler
+export const logOut = async (req, res) => {
+    try {
+        res.clearCookie("token");
+        res
+        .status(200)
+        .json({ status: "success", message: "User signed out successfully" });
+    } catch (error) {
+        res
+        .status(500)
+        .json({ error: "Server error", errorMessage: error.message });
+    }
+};
