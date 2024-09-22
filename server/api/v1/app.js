d87b7e4de84dea254dd2c5347c7e4968b7aad87e
Lamber22 <Lamberjoe5@gmail.com>
2024-09-22 18:35:27 +0000

    Set up Express server with middleware and routes

---
+++ server/api/v1/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import morgan from "morgan";
+import cors from "cors";
+import dbClient from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import taskRouter from "./routes/taskRoutes.js";
+
+const app = express();
+
+// Middleware for parsing JSON request bodies
+app.use(express.json());
+
+app.get("/api/v1/", (req, res) => {
+    res.status(200).json({ message: "Hello from Task Management API" });
+});
+app.use(morgan("dev"));
+app.use(cors());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/task", taskRouter);
+
+export default app;
