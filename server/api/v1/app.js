872abfab93c78bc0ddcf6d88bdf6bdd027509043
Lamber22 <Lamberjoe5@gmail.com>
2024-09-23 01:27:33 +0000

    feat: Add Athentication middleware

--- server/api/v1/app.js
+++ server/api/v1/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dbClient from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
 import taskRouter from "./routes/taskRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -18,5 +19,6 @@ app.use(cors());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/task", taskRouter);
+app.use("/api/v1/user", userRouter);
 
 export default app;
