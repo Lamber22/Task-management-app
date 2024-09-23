16e6509ac5b6c47a414e61f147cddd3e0186b5fa
Lamber22 <Lamberjoe5@gmail.com>
2024-09-23 01:30:41 +0000

    Revert "feat: Add Athentication middleware and add user controller"
    This reverts commit 872abfab93c78bc0ddcf6d88bdf6bdd027509043.

--- server/api/v1/app.js
+++ server/api/v1/app.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import dbClient from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
 import taskRouter from "./routes/taskRoutes.js";
-import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -19,6 +18,5 @@ app.use(cors());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/task", taskRouter);
-app.use("/api/v1/user", userRouter);
 
 export default app;
