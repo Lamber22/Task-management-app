0ad3ca205229ea0c5003691c42a65d1954835995
Lamber22 <Lamberjoe5@gmail.com>
2024-09-23 01:21:07 +0000

    feat: Improve endpoints with informative response

--- server/api/v1/controllers/taskController.js
+++ server/api/v1/controllers/taskController.js
@@ -7,7 +7,7 @@ export const createTask = async (req, res) => {
         const { title, description, dueDate, category, status } = req.body;
 
         const task = new Task({
-        user: req.user.id,
+        user: req.user.userId,
         title,
         description,
         dueDate,
@@ -16,34 +16,68 @@ export const createTask = async (req, res) => {
         });
 
         await task.save();
-        res.json(task);
+        res.status(201).json({
+            status: "success",
+            message: "Task created successfully",
+            data: task,
+        });
     } catch (err) {
-        res.status(500).json({ msg: 'Server error' });
+        console.error(err);
+        res.status(500).json({ message: 'Server error', error: err });
     }
 };
 
 // Get All Tasks
 export const getTasks = async (req, res) => {
     try {
-        const tasks = await Task.find({ user: req.user.id });
-        res.json(tasks);
+        const tasks = await Task.find({ user: req.user.userId });
+        if (tasks.length === 0) return res.json("No task found");
+        res.status(200).json({
+            status: "success",
+            message: "Tasks retrieved successfully",
+            numTasks: tasks.length,
+            data: tasks,
+        });
     } catch (err) {
-        res.status(500).json({ msg: 'Server error' });
+        res.status(500).json({ message: 'Server error' });
     }
 };
 
+// Get Task by ID
+export const getTaskByID = async (req, res) => {
+    try {
+        let task = await Task.findById(req.params.id);
+        if (!task) {
+        return res.status(404).json({ message: 'Task not found' });
+        }
+
+        res.status(200).json({
+            status: "success",
+            message: "Task retrieved successfully",
+            data: task,
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error', error: err });
+    }
+};
 // Update Task
 export const updateTask = async (req, res) => {
     try {
         let task = await Task.findById(req.params.id);
         if (!task) {
-        return res.status(404).json({ msg: 'Task not found' });
+        return res.status(404).json({ message: 'Task not found' });
         }
 
         task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        res.json(task);
+        res.status(200).json({
+            status: "success",
+            message: "Task updated successfully",
+            data: task,
+        });
     } catch (err) {
-        res.status(500).json({ msg: 'Server error' });
+        console.error(err);
+        res.status(500).json({ message: 'Server error', error: err });
     }
 };
 
@@ -52,12 +86,15 @@ export const deleteTask = async (req, res) => {
     try {
         const task = await Task.findById(req.params.id);
         if (!task) {
-        return res.status(404).json({ msg: 'Task not found' });
+        return res.status(404).json({ message: 'Task not found' });
         }
 
         await Task.findByIdAndDelete(req.params.id);
-        res.json({ msg: 'Task deleted' });
+        res.status(200).json({
+            status: "success",
+            message: "Task deleted successfully",
+        });
     } catch (err) {
-        res.status(500).json({ msg: 'Server error' });
+        res.status(500).json({ message: 'Server error' });
     }
 };
