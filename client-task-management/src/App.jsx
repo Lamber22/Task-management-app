e0aea1e2045cb618ab256bddb38d210afb2baea7
Lamber22 <Lamberjoe5@gmail.com>
2024-09-24 14:07:00 +0000

    Add analytics

--- client-task-management/src/App.jsx
+++ client-task-management/src/App.jsx
@@ -2,10 +2,12 @@ import { RegistrationForm, Login } from "./components";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <div>
+      <Analytics/>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
